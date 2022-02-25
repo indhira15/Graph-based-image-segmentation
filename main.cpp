29c8df316ee1a91cc8be7297f863c432e21a415e
indhira15 <indhira.ramirez@utec.edu.pe>
2022-02-24 23:29:35 -0500

    added segment all function

--- main.cpp
+++ main.cpp
@@ -13,32 +13,36 @@ using namespace std;
 void segment_all(int k){
 	fstream file;
 	string line;
-
+	pair<int,int> p;
 	file.open("images_list.txt");
 	while(file >> line){
 		Grafo gf("images_temporal/" + line + ".in");
-		gf.Segmentar(k);
-		gf.Make_image("images_temporal/" + line +"_" + to_string(k) + ".out");
+		p = gf.Segmentar(k);
+		gf.Make_image("images_temporal/" + line +"_k_" + to_string(k) + "init_clust=" + to_string(p.first) +  "_final_clust=" + to_string(p.second)  + ".out");
 	}
 }
 
 void segment_all(vector<int> ks){
 	fstream file;
 	string line;
-
+	pair<int,int> p;
 	file.open("images_list.txt");
 	while(file >> line){
 		for(auto &i:ks){
+			cout << "segmenting " << line << " with k " << i<< endl;
 			Grafo gf("images_temporal/" + line + ".in");
-			gf.Segmentar(i);
-			gf.Make_image("images_temporal/" + line + "_" + to_string(i) + ".out");
+			p = gf.Segmentar(i);
+			gf.Make_image("images_temporal/" + line +"_k_" + to_string(i) + "init_clust=" + to_string(p.first) +  "_final_clust=" + to_string(p.second)  + ".out");
 		}
 	}
+
 }
 
 int main(){
 
-	segment_all(1100);
+	vector<int> v{500,750,1000,1250,1500,2000,3000,4000,5000,10000};
+	segment_all(v);
+
 
 	return 0;
 }
